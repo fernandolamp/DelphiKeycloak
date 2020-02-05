c37b7d1ef7ba2dac110c2e3be78b146195bec5a4
Cabelo <lampada@gmail.com>
2020-02-05 00:56:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Delphi + Keycloak Simple Example
+
+Example how to get a authentication token from keycloak using openid with grant type password.
+
+## What i need to run the project?
+
+1 - Configure a keycloack, you can use a docker image https://hub.docker.com/r/jboss/keycloak/
+
+2 - Use the well known url of openId and paste on the field of formulary
+
+3 - Fill the field username and passworkd
+
+4 - Click on getToken
+
+You can validate the token on jwt.io
+
+
+
+
+
+
+
+
---
+++ TPrincipal.pas
@@ -0,0 +1,91 @@
+unit TPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, REST.Types, Data.Bind.Components,
+  Data.Bind.ObjectScope, REST.Client, Vcl.StdCtrls,System.JSON,Vcl.Clipbrd;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    edtUsername: TEdit;
+    edtPassword: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    edtWellKnown: TEdit;
+    Label3: TLabel;
+    RESTClient1: TRESTClient;
+    RESTRequest1: TRESTRequest;
+    RESTResponse1: TRESTResponse;
+    edtClientId: TEdit;
+    Label4: TLabel;
+    memoToken: TMemo;
+    CopyToken: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure CopyTokenClick(Sender: TObject);
+  private
+    function GetJsonValue(psValue, psJson: String):String;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  sTokenEndpoint:String;
+  sJwksUri:String;
+begin
+  RESTClient1.BaseURL := edtWellKnown.Text;
+  RESTRequest1.Execute;
+
+  if not RESTResponse1.Status.Success then
+    raise Exception.Create('Sorry, was unable to get token url');
+
+  //first, get token url
+  RESTResponse1.GetSimpleValue('token_endpoint',sTokenEndpoint);
+  //get certificate to validate token on jwt.io
+  RestResponse1.GetSimpleValue('jwks_uri',sJwksUri);
+
+  //Set openid params to authenticate
+  RESTClient1.ResetToDefaults;
+  RESTClient1.BaseURL := sTokenEndpoint;
+  RESTRequest1.Method := TRESTRequestMethod.rmPOST;
+  RESTRequest1.AddParameter('client_id',edtClientId.Text,TRESTRequestParameterKind.pkGETorPOST);
+  RESTRequest1.AddParameter('grant_type','password',TRESTRequestParameterKind.pkGETorPOST);
+  Restrequest1.AddParameter('username',edtusername.Text,TRESTRequestParameterKind.pkGETorPOST);
+  Restrequest1.AddParameter('password',edtPassword.Text,TRESTRequestParameterKind.pkGETorPOST);
+
+  RESTRequest1.Execute;
+  memoToken.Text := RESTRequest1.Response.Content;
+end;
+
+procedure TForm1.CopyTokenClick(Sender: TObject);
+begin
+  ClipBoard.AsText := GetJsonValue('access_token',memoToken.Text);
+end;
+
+function TForm1.GetJsonValue(psValue:String;psJson:String):string;
+var
+  json:TJSONObject;
+  jsonValue:TJsonValue;
+begin
+  json := TJSONObject.Create;
+  try
+    jsonValue := json.ParseJSONValue(psJson);
+    result := jsonValue.FindValue(psvalue).Value;
+  finally
+    FreeAndNil(json);
+  end;
+
+end;
+
+end.
